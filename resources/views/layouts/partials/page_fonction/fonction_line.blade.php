3e35b105cc9d8bca920f0c3abd3bf7c41024ffea
Manoah VERDIER <verdier.developpement@gmail.com>
2020-09-22 14:20:43 +0200

    css

--- resources/views/layouts/partials/page_fonction/fonction_line.blade.php
+++ resources/views/layouts/partials/page_fonction/fonction_line.blade.php
@@ -7,6 +7,6 @@
     <img src="storage/{{$img}}">
 </div>
 <div class="test-right col-12 d-none @if($side=="left") d-md-none @else d-md-block @endif col-md-6 my-3">
-    <h5>{{$title}}</h5>
-    <p>{{$content}}</p>
+    <h5 class="text-primary font-weight-bold border-bottom">{{$title}}</h5>
+    <p class="text-center text-md-left">{{$content}}</p>
 </div>
