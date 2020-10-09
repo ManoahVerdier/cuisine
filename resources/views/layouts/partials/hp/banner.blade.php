28604db6e0919a36202890dba21fa73e77a6d57a
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-09 15:57:49 +0200

    confiance

--- resources/views/layouts/partials/hp/banner.blade.php
+++ resources/views/layouts/partials/hp/banner.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid @if($space ?? false) mt-5 @endif" id='banner'>
     <div class="row bg-primary pt-2">
-        <div class="col-4 d-none d-md-block">
+        <div class="col-5 d-none d-md-block">
             <div class="banner-text p-3 text-white">
                 {!!$text!!}
             </div>
@@ -12,9 +12,9 @@
                 @endif
             </div>
         </div>
-        <div class="col-12 col-md-8 align-self-center mb-5">
-            <div class="banner-img">
-                <img class="w-100" src="{{$img}}"/>
+        <div class="col-12 col-md-7 align-self-center mb-5 px-5">
+            <div class="banner-img px-5">
+                <img class="w-100 px-5" src="{{$img}}"/>
             </div>
         </div>
         <div class="col-12 d-md-none">
--- resources/views/layouts/partials/hp/confiance.blade.php
+++ resources/views/layouts/partials/hp/confiance.blade.php
@@ -1,27 +1,27 @@
 <div class="container-fluid mt-4" id="confiance">
-    <div class="row bg-secondary">
+    <div class="row bg-secondary px-5 pt-3">
         <div class="col-12 mb-3 h3 text-left text-white">
             Ils nous font confiance
         </div>
     </div>
-    <div class="row bg-secondary">
-        <div class="col-4 col-md-2 mb-3">
+    <div class="row bg-secondary px-5">
+        <div class="col-4 col-md-2 mb-3 px-5">
             <img class="w-100" src="{{asset('/images/HP/confiance1.png')}}"/>
         </div>
-        <div class="col-4 col-md-2 mb-3">
+        <div class="col-4 col-md-2 mb-3 px-5">
             <img class="w-100" src="{{asset('/images/HP/confiance2.png')}}"/>
         </div>
-        <div class="col-4 col-md-2 mb-3">
+        <div class="col-4 col-md-2 mb-3 px-5">
             <img class="w-100" src="{{asset('/images/HP/confiance3.png')}}"/>
         </div>
-        <div class="col-4 col-md-2 mb-3">
+        <div class="col-4 col-md-2 mb-3 px-5">
             <img class="w-100" src="{{asset('/images/HP/confiance4.png')}}"/>
         </div>
-        <div class="col-4 col-md-2 mb-3">
+        <div class="col-4 col-md-2 mb-3 px-5">
             <img class="w-100" src="{{asset('/images/HP/confiance5.png')}}"/>
         </div>
     </div>
-    <div class="row">
+    <div class="row mt-3">
         <div class="col-12  mt-3  h3 text-left black">
             {!!$hp->confiance_text!!}
         </div>
