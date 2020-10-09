c7ba61a7891591d06ecdf866389cfb96a5092974
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-09 16:04:15 +0200

    demo

--- resources/views/layouts/partials/hp/demo.blade.php
+++ resources/views/layouts/partials/hp/demo.blade.php
@@ -1,10 +1,10 @@
 <div class="container-fluid bg-primary @if($space ?? false) mt-5 @endif" id='demo'>
     <div class="row py-4 pl-2">
-        <div class="align-self-center text-white col-md-8 col-12 pl-md-4 text-center text-md-left h4">
+        <div class="align-self-center text-white col-md-8 col-12 pl-md-4 text-center text-md-left h4 mb-0">
             {!!$text!!}
         </div>
         <div class="align-self-center col-md-4 col-12 text-center">
-            <a href="{{route('contact',['demo'=>1])}}" class="btn btn-light text-primary font-weight-bold">
+            <a href="{{route('contact',['demo'=>1])}}" class="btn btn-lg btn-light text-primary font-weight-bold">
                 Demander une démo
             </a>
         </div>
