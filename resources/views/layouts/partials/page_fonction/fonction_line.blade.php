a8e06ace6978fa99be3989b28eecf321fc30b0f9
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-09 09:25:43 +0200

    test

--- resources/views/layouts/partials/page_fonction/fonction_line.blade.php
+++ resources/views/layouts/partials/page_fonction/fonction_line.blade.php
@@ -4,7 +4,7 @@
     <p class="text-center text-md-left">{{$content}}</p>
 </div>
 <div class="img col-12 col-md-6 my-3 text-center">
-    <img src="storage/{{$img}}">
+    <img class="w-100" src="storage/{{$img}}">
 </div>
 <div class="test-right col-12 d-none @if($side=="left") d-md-none @else d-md-block @endif col-md-6 my-3">
     <h5 class="text-primary font-weight-bold border-bottom">{{$title}}</h5>
