b2b09a6f689cab2389566fcff058b47d627ff769
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-09 16:02:22 +0200

    connexion

--- resources/views/layouts/partials/hp/connexion.blade.php
+++ resources/views/layouts/partials/hp/connexion.blade.php
@@ -1,11 +1,11 @@
 <div class="container-fluid" id='connexion'>
     <div class="row bg-primary pt-2">
-        <div class="col-12 col-md-7">
+        <div class="col-12 col-md-9">
             <div class="connexion-text p-3 text-white text-center h4">
                 {!!$hp->connexion_text!!}
             </div>
         </div>
-        <div class="col-12 col-md-5 align-self-center mb-5">
+        <div class="col-12 col-md-3 align-self-center mb-5">
             <div class="connexion-img">
                 <img class="w-100" src="{{asset('/images/HP/connexion.jpg')}}"/>
             </div>
