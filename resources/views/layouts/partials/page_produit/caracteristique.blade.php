7054d6477fe6e3dda31725f387d4b2668a9285da
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-09 16:38:15 +0200

    test

--- resources/views/layouts/partials/page_produit/caracteristique.blade.php
+++ resources/views/layouts/partials/page_produit/caracteristique.blade.php
@@ -1,6 +1,6 @@
 <div class="col-12 col-md-6 mb-4">
     <div class="row">
-        <div class="col-2">
+        <div class="col-md-1 offset-md-1 col-2">
             <div class="w-100 h-100 gear"></div>
         </div>
         <div class="col-10">
