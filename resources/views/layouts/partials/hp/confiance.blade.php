15e3fe1a5a3521e3d15865205674f8246db8c23d
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-09 09:33:44 +0200

    confiance

--- resources/views/layouts/partials/hp/confiance.blade.php
+++ resources/views/layouts/partials/hp/confiance.blade.php
@@ -6,22 +6,19 @@
     </div>
     <div class="row">
         <div class="col-4 col-md-2 mb-3">
-            <img class="w-100" src="{{asset('/images/HP/screenshot.png')}}"/>
+            <img class="w-100" src="{{asset('/images/HP/confiance1.png')}}"/>
         </div>
         <div class="col-4 col-md-2 mb-3">
-            <img class="w-100" src="{{asset('/images/HP/screenshot.png')}}"/>
+            <img class="w-100" src="{{asset('/images/HP/confiance2.png')}}"/>
         </div>
         <div class="col-4 col-md-2 mb-3">
-            <img class="w-100" src="{{asset('/images/HP/screenshot.png')}}"/>
+            <img class="w-100" src="{{asset('/images/HP/confiance3.png')}}"/>
         </div>
         <div class="col-4 col-md-2 mb-3">
-            <img class="w-100" src="{{asset('/images/HP/screenshot.png')}}"/>
+            <img class="w-100" src="{{asset('/images/HP/confiance4.png')}}"/>
         </div>
         <div class="col-4 col-md-2 mb-3">
-            <img class="w-100" src="{{asset('/images/HP/screenshot.png')}}"/>
-        </div>
-        <div class="col-4 col-md-2 mb-3">
-            <img class="w-100" src="{{asset('/images/HP/screenshot.png')}}"/>
+            <img class="w-100" src="{{asset('/images/HP/confiance5.png')}}"/>
         </div>
     </div>
     <div class="row">
