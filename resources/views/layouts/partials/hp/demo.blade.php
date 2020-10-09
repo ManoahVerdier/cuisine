e077d03adcf198e9865003c3e8627107473d2b57
Manoah VERDIER <verdier.developpement@gmail.com>
2020-10-09 16:03:17 +0200

    demo

--- resources/views/layouts/partials/hp/demo.blade.php
+++ resources/views/layouts/partials/hp/demo.blade.php
@@ -1,6 +1,6 @@
 <div class="container-fluid bg-primary @if($space ?? false) mt-5 @endif" id='demo'>
     <div class="row py-4 pl-2">
-        <div class="align-self-center text-white col-md-8 col-12 pl-md-4 text-center text-md-left">
+        <div class="align-self-center text-white col-md-8 col-12 pl-md-4 text-center text-md-left h4">
             {!!$text!!}
         </div>
         <div class="align-self-center col-md-4 col-12 text-center">
